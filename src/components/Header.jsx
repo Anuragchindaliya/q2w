c19d93456e846403250fb5dc0ae628548e90e5c9
Anuragchindaliya <ak1148383@gmail.com>
2022-02-13 19:41:24 +0530

    fix: logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
     return <nav className="navbar navbar-dark bg-dark navbar-fixed-top text-center">
         <div className="container-fluid">
             <div className="navbar-header">
-                <a className="navbar-brand" href="#/">  <img src='/assets/img/q2w-logo.svg' style={{ width: "50px", padding: "0 5px" }} alt="Brand logo" />
+                <a className="navbar-brand" href="#/">  <img src='/q2w/assets/img/q2w-logo.svg' style={{ width: "50px", padding: "0 5px" }} alt="Brand logo" />
                     Query-2-Write
                 </a>
                 {/* D:\react\q2w\public\assets\img\q2w-logo.svg */}
